4cbb299a34441bad2ea26c053f97fb461bdacbd3
RCS Mac <rcsmac@RCSs-iMac.local>
2017-03-28 14:39:19 +0530

    Singleton template implemented

---
+++ Custom Templates/SINGLETON.xctemplate/___FILEBASENAME___/___FILEBASENAME___Singleton.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+class ___FILEBASENAMEASIDENTIFIER___Singleton {
+    
+    static let sharedInstance = ___FILEBASENAMEASIDENTIFIER___Singleton()
+    
+    private init() {}
+    
+}
+
